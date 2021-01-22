5764be6fd727f3104249783d44ab39248e7708e6
chengxb <chengxb@53kf.com>
2021-01-22 17:17:08 +0800

    可以训练模型

--- test/03_short_text/trainModel.py
+++ test/03_short_text/trainModel.py
@@ -17,6 +17,7 @@ import numpy as np
 from collections import Counter
 import json
 import tensorflow.compat.v1 as tf
+import datetime
 
 from common.log import Log
 from common.utils import removeFileIfExists
@@ -35,7 +36,7 @@ file_labeled_train_data_csv = os.path.join(basePath, 'data/labeledTrainData.csv'
 # 输出数据：词向量文件
 bin_word2Vec = os.path.join(basePath, 'data/word2Vec.bin')
 
-#词向量维度参数设置
+# 词向量维度参数设置
 vector_size = 200
 
 
@@ -330,6 +331,9 @@ class Dataset(object):
 
         f_word2idx_json = os.path.join(basePath, 'data/word2idx.json')
         f_label2idx_json = os.path.join(basePath, 'data/label2idx.json')
+        removeFileIfExists(f_word2idx_json)
+        removeFileIfExists(f_label2idx_json)
+
         # 将词汇-索引映射表保存为json数据，之后做inference时直接加载来处理数据
         with open(f_word2idx_json, "w", encoding="utf-8") as f:
             json.dump(word2idx, f, ensure_ascii=False)
@@ -778,166 +782,167 @@ evalLabels = data.evalLabels
 wordEmbedding = data.wordEmbedding
 labelList = data.labelList
 
-# # 定义计算图
-# with tf.Graph().as_default():
-#     session_conf = tf.ConfigProto(allow_soft_placement=True, log_device_placement=False)
-#     session_conf.gpu_options.allow_growth = True
-#     session_conf.gpu_options.per_process_gpu_memory_fraction = 0.9  # 配置gpu占用率
-#
-#     sess = tf.Session(config=session_conf)
-#
-#     # 定义会话
-#     with sess.as_default():
-#         lstm = BiLSTMAttention(config, wordEmbedding)
-#
-#         globalStep = tf.Variable(0, name="globalStep", trainable=False)
-#         # 定义优化函数，传入学习速率参数
-#         optimizer = tf.train.AdamOptimizer(config.training.learningRate)
-#         # 计算梯度,得到梯度和变量
-#         gradsAndVars = optimizer.compute_gradients(lstm.loss)
-#         # 将梯度应用到变量下，生成训练器
-#         trainOp = optimizer.apply_gradients(gradsAndVars, global_step=globalStep)
-#
-#         # 用summary绘制tensorBoard
-#         gradSummaries = []
-#         for g, v in gradsAndVars:
-#             if g is not None:
-#                 tf.summary.histogram("{}/grad/hist".format(v.name), g)
-#                 tf.summary.scalar("{}/grad/sparsity".format(v.name), tf.nn.zero_fraction(g))
-#
-#         # outDir = os.path.abspath(os.path.join(os.path.curdir, "summarys"))
-#         outDir = os.path.join(basePath, 'data/model/summarys')
-#         log.debug("Writing to {}\n".format(outDir))
-#
-#         lossSummary = tf.summary.scalar("loss", lstm.loss)
-#         summaryOp = tf.summary.merge_all()
-#
-#         trainSummaryDir = os.path.join(outDir, "train")
-#         trainSummaryWriter = tf.summary.FileWriter(trainSummaryDir, sess.graph)
-#
-#         evalSummaryDir = os.path.join(outDir, "eval")
-#         evalSummaryWriter = tf.summary.FileWriter(evalSummaryDir, sess.graph)
-#
-#         # 初始化所有变量
-#         saver = tf.train.Saver(tf.global_variables(), max_to_keep=5)
-#
-#         # 保存模型的一种方式，保存为pb文件
-#         # savedModelPath = "../model/bilstm-atten/savedModel"
-#         savedModelPath = os.path.join(basePath, "data/model/savedModel")
-#         if os.path.exists(savedModelPath):
-#             os.rmdir(savedModelPath)
-#         builder = tf.saved_model.builder.SavedModelBuilder(savedModelPath)
-#
-#         sess.run(tf.global_variables_initializer())
-#
-#
-#         def trainStep(batchX, batchY):
-#             """
-#             训练函数
-#             """
-#             feed_dict = {
-#                 lstm.inputX: batchX,
-#                 lstm.inputY: batchY,
-#                 lstm.dropoutKeepProb: config.model.dropoutKeepProb
-#             }
-#             _, summary, step, loss, predictions = sess.run(
-#                 [trainOp, summaryOp, globalStep, lstm.loss, lstm.predictions],
-#                 feed_dict)
-#             timeStr = datetime.datetime.now().isoformat()
-#
-#             if config.numClasses == 1:
-#                 acc, recall, prec, f_beta = get_binary_metrics(pred_y=predictions, true_y=batchY)
-#
-#
-#             elif config.numClasses > 1:
-#                 acc, recall, prec, f_beta = get_multi_metrics(pred_y=predictions, true_y=batchY,
-#                                                               labels=labelList)
-#
-#             trainSummaryWriter.add_summary(summary, step)
-#
-#             return loss, acc, prec, recall, f_beta
-#
-#
-#         def devStep(batchX, batchY):
-#             """
-#             验证函数
-#             """
-#             feed_dict = {
-#                 lstm.inputX: batchX,
-#                 lstm.inputY: batchY,
-#                 lstm.dropoutKeepProb: 1.0
-#             }
-#             summary, step, loss, predictions = sess.run(
-#                 [summaryOp, globalStep, lstm.loss, lstm.predictions],
-#                 feed_dict)
-#
-#             if config.numClasses == 1:
-#
-#                 acc, precision, recall, f_beta = get_binary_metrics(pred_y=predictions, true_y=batchY)
-#             elif config.numClasses > 1:
-#                 acc, precision, recall, f_beta = get_multi_metrics(pred_y=predictions, true_y=batchY, labels=labelList)
-#
-#             evalSummaryWriter.add_summary(summary, step)
-#
-#             return loss, acc, precision, recall, f_beta
-#
-#
-#         for i in range(config.training.epoches):
-#             # 训练模型
-#             log.debug("start training model")
-#             for batchTrain in nextBatch(trainReviews, trainLabels, config.batchSize):
-#                 loss, acc, prec, recall, f_beta = trainStep(batchTrain[0], batchTrain[1])
-#
-#                 currentStep = tf.train.global_step(sess, globalStep)
-#                 log.debug("train: step: {}, loss: {}, acc: {}, recall: {}, precision: {}, f_beta: {}".format(
-#                     currentStep, loss, acc, recall, prec, f_beta))
-#                 if currentStep % config.training.evaluateEvery == 0:
-#                     log.debug("\nEvaluation:")
-#
-#                     losses = []
-#                     accs = []
-#                     f_betas = []
-#                     precisions = []
-#                     recalls = []
-#
-#                     for batchEval in nextBatch(evalReviews, evalLabels, config.batchSize):
-#                         loss, acc, precision, recall, f_beta = devStep(batchEval[0], batchEval[1])
-#                         losses.append(loss)
-#                         accs.append(acc)
-#                         f_betas.append(f_beta)
-#                         precisions.append(precision)
-#                         recalls.append(recall)
-#
-#                     time_str = datetime.datetime.now().isoformat()
-#                     log.debug("{}, step: {}, loss: {}, acc: {},precision: {}, recall: {}, f_beta: {}".format(time_str,
-#                                                                                                              currentStep,
-#                                                                                                              mean(
-#                                                                                                                  losses),
-#                                                                                                              mean(accs),
-#                                                                                                              mean(
-#                                                                                                                  precisions),
-#                                                                                                              mean(
-#                                                                                                                  recalls),
-#                                                                                                              mean(
-#                                                                                                                  f_betas)))
-#
-#                 if currentStep % config.training.checkpointEvery == 0:
-#                     # 保存模型的另一种方法，保存checkpoint文件
-#                     path = saver.save(sess, os.path.join(basePath, "data/model/"), global_step=currentStep)
-#                     log.debug("Saved model checkpoint to {}\n".format(path))
-#
-#         inputs = {"inputX": tf.saved_model.utils.build_tensor_info(lstm.inputX),
-#                   "keepProb": tf.saved_model.utils.build_tensor_info(lstm.dropoutKeepProb)}
-#
-#         outputs = {"predictions": tf.saved_model.utils.build_tensor_info(lstm.predictions)}
-#
-#         prediction_signature = tf.saved_model.signature_def_utils.build_signature_def(inputs=inputs, outputs=outputs,
-#                                                                                       method_name=tf.saved_model.signature_constants.PREDICT_METHOD_NAME)
-#         legacy_init_op = tf.group(tf.tables_initializer(), name="legacy_init_op")
-#         builder.add_meta_graph_and_variables(sess, [tf.saved_model.tag_constants.SERVING],
-#                                              signature_def_map={"predict": prediction_signature},
-#                                              legacy_init_op=legacy_init_op)
-#
-#         builder.save()
-#
-# log.info('训练完成')
+# 定义计算图
+with tf.Graph().as_default():
+    session_conf = tf.ConfigProto(allow_soft_placement=True, log_device_placement=False)
+    session_conf.gpu_options.allow_growth = True
+    session_conf.gpu_options.per_process_gpu_memory_fraction = 0.9  # 配置gpu占用率
+
+    sess = tf.Session(config=session_conf)
+
+    # 定义会话
+    with sess.as_default():
+        lstm = BiLSTMAttention(config, wordEmbedding)
+
+        globalStep = tf.Variable(0, name="globalStep", trainable=False)
+        # 定义优化函数，传入学习速率参数
+        optimizer = tf.train.AdamOptimizer(config.training.learningRate)
+        # 计算梯度,得到梯度和变量
+        gradsAndVars = optimizer.compute_gradients(lstm.loss)
+        # 将梯度应用到变量下，生成训练器
+        trainOp = optimizer.apply_gradients(gradsAndVars, global_step=globalStep)
+
+        # 用summary绘制tensorBoard
+        gradSummaries = []
+        for g, v in gradsAndVars:
+            if g is not None:
+                tf.summary.histogram("{}/grad/hist".format(v.name), g)
+                tf.summary.scalar("{}/grad/sparsity".format(v.name), tf.nn.zero_fraction(g))
+
+        # outDir = os.path.abspath(os.path.join(os.path.curdir, "summarys"))
+        outDir = os.path.join(basePath, 'data/model/summarys')
+        log.debug("Writing to {}\n".format(outDir))
+
+        lossSummary = tf.summary.scalar("loss", lstm.loss)
+        summaryOp = tf.summary.merge_all()
+
+        trainSummaryDir = os.path.join(outDir, "train")
+        trainSummaryWriter = tf.summary.FileWriter(trainSummaryDir, sess.graph)
+
+        evalSummaryDir = os.path.join(outDir, "eval")
+        evalSummaryWriter = tf.summary.FileWriter(evalSummaryDir, sess.graph)
+
+        # 初始化所有变量
+        saver = tf.train.Saver(tf.global_variables(), max_to_keep=5)
+
+        # 保存模型的一种方式，保存为pb文件
+        # savedModelPath = "../model/bilstm-atten/savedModel"
+        savedModelPath = os.path.join(basePath, "data/model/savedModel")
+        if os.path.exists(savedModelPath):
+            os.rmdir(savedModelPath)
+        builder = tf.saved_model.builder.SavedModelBuilder(savedModelPath)
+
+        sess.run(tf.global_variables_initializer())
+
+
+        def trainStep(batchX, batchY):
+            """
+            训练函数
+            """
+            feed_dict = {
+                lstm.inputX: batchX,
+                lstm.inputY: batchY,
+                lstm.dropoutKeepProb: config.model.dropoutKeepProb
+            }
+            _, summary, step, loss, predictions = sess.run(
+                [trainOp, summaryOp, globalStep, lstm.loss, lstm.predictions],
+                feed_dict)
+            timeStr = datetime.datetime.now().isoformat()
+
+            # 二分类的代码
+            # if config.numClasses == 1:
+            #     acc, recall, prec, f_beta = get_binary_metrics(pred_y=predictions, true_y=batchY)
+
+            # elif config.numClasses > 1:
+            acc, recall, prec, f_beta = get_multi_metrics(pred_y=predictions, true_y=batchY,
+                                                          labels=labelList)
+
+            trainSummaryWriter.add_summary(summary, step)
+
+            return loss, acc, prec, recall, f_beta
+
+
+        def devStep(batchX, batchY):
+            """
+            验证函数
+            """
+            feed_dict = {
+                lstm.inputX: batchX,
+                lstm.inputY: batchY,
+                lstm.dropoutKeepProb: 1.0
+            }
+            summary, step, loss, predictions = sess.run(
+                [summaryOp, globalStep, lstm.loss, lstm.predictions],
+                feed_dict)
+
+            if config.numClasses == 1:
+
+                acc, precision, recall, f_beta = get_binary_metrics(pred_y=predictions, true_y=batchY)
+            elif config.numClasses > 1:
+                acc, precision, recall, f_beta = get_multi_metrics(pred_y=predictions, true_y=batchY, labels=labelList)
+
+            evalSummaryWriter.add_summary(summary, step)
+
+            return loss, acc, precision, recall, f_beta
+
+
+        for i in range(config.training.epoches):
+            # 训练模型
+            log.debug("epoches：{}{}".format(i, config.training.epoches))
+            for batchTrain in nextBatch(trainReviews, trainLabels, config.batchSize):
+                loss, acc, prec, recall, f_beta = trainStep(batchTrain[0], batchTrain[1])
+
+                currentStep = tf.train.global_step(sess, globalStep)
+                log.debug("train: step: {}, loss: {}, acc: {}, recall: {}, precision: {}, f_beta: {}".format(
+                    currentStep, loss, acc, recall, prec, f_beta))
+
+                if currentStep % config.training.evaluateEvery == 0:
+                    log.debug("\nEvaluation:")
+
+                    losses = []
+                    accs = []
+                    f_betas = []
+                    precisions = []
+                    recalls = []
+
+                    for batchEval in nextBatch(evalReviews, evalLabels, config.batchSize):
+                        loss, acc, precision, recall, f_beta = devStep(batchEval[0], batchEval[1])
+                        losses.append(loss)
+                        accs.append(acc)
+                        f_betas.append(f_beta)
+                        precisions.append(precision)
+                        recalls.append(recall)
+
+                    time_str = datetime.datetime.now().isoformat()
+                    log.debug("{}, step: {}, loss: {}, acc: {},precision: {}, recall: {}, f_beta: {}".format(time_str,
+                                                                                                             currentStep,
+                                                                                                             mean(
+                                                                                                                 losses),
+                                                                                                             mean(accs),
+                                                                                                             mean(
+                                                                                                                 precisions),
+                                                                                                             mean(
+                                                                                                                 recalls),
+                                                                                                             mean(
+                                                                                                                 f_betas)))
+
+                if currentStep % config.training.checkpointEvery == 0:
+                    # 保存模型的另一种方法，保存checkpoint文件
+                    path = saver.save(sess, os.path.join(basePath, "data/model/"), global_step=currentStep)
+                    log.debug("Saved model checkpoint to {}\n".format(path))
+
+        inputs = {"inputX": tf.saved_model.utils.build_tensor_info(lstm.inputX),
+                  "keepProb": tf.saved_model.utils.build_tensor_info(lstm.dropoutKeepProb)}
+
+        outputs = {"predictions": tf.saved_model.utils.build_tensor_info(lstm.predictions)}
+
+        prediction_signature = tf.saved_model.signature_def_utils.build_signature_def(inputs=inputs, outputs=outputs,
+                                                                                      method_name=tf.saved_model.signature_constants.PREDICT_METHOD_NAME)
+        legacy_init_op = tf.group(tf.tables_initializer(), name="legacy_init_op")
+        builder.add_meta_graph_and_variables(sess, [tf.saved_model.tag_constants.SERVING],
+                                             signature_def_map={"predict": prediction_signature},
+                                             legacy_init_op=legacy_init_op)
+
+        builder.save()
+
+log.info('训练完成')

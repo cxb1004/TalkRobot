889f6b8fc54d62a7bcb91eb099f2021de3429855
chengxb <chengxb@53kf.com>
2021-02-03 18:32:54 +0800

    0203下班备份  进行语料库整理工作

--- test/05_wedding_photo_corpus/prepareTrainData.py
+++ test/05_wedding_photo_corpus/prepareTrainData.py
@@ -19,6 +19,29 @@ tag_answer = os.path.join(basePath, 'data/tag_answer.json')
 tag_question = os.path.join(basePath, 'data/tag_question.json')
 labeled_train_data_csv = os.path.join(basePath, 'data/labeledTrainData.csv')
 
+
+def add_question_under_tag(tag_question, tag, q):
+    """
+    TODO
+    把问题加入到标签下的问题列表中
+    1、先检查tag_question是否存在标签，不存在就做插入操作
+    2、如果存在，去除问题列表对象，查看q是否已经存在于列表，存在则抛出警告，不存在就插入操作
+    :param tag_question:标签以及标签下所有问题的集合{标签：问题列表}
+    :param tag:标签
+    :param q:需要新增的问题
+    :return:
+    """
+    if tag_question.get(tag) is not None:
+        questions = tag_question[tag]
+        questions.insert(q)
+        tag_question[tag] = questions
+    else:
+        questions = list()
+        questions.append(q)
+        tag_question[tag] = questions
+    pass
+
+
 with open(source_file, 'r+', encoding='utf-8') as f:
     # 载入json内容
     jsonContent = json.load(f)
@@ -28,8 +51,8 @@ with open(source_file, 'r+', encoding='utf-8') as f:
     # 获得data节点的内容
     all_question = jsonContent['data']
 
-    dict_tag = {}
-    tag_question = []
+    tag_desc_answer = dict()
+    tag_question = dict()
     cnt_std_questions = 0
     cnt_sim_questions = 0
     for question in all_question:
@@ -48,15 +71,19 @@ with open(source_file, 'r+', encoding='utf-8') as f:
         if is_std_question == 1:
             # 如果是标准问题
             tag = question['knowledge_id']
-            desc = question['question']
+            q = question['question']
+            # ?? answer需要去标签化
             answer = question['answer']
 
-            if dict_tag.has_key(tag):
+            if tag_desc_answer.get(tag) is not None:
                 log.warn('重复标签：  \"knowledge_id\": \"{}\"'.format(tag))
             else:
-                dict_tag[tag] = desc
-        else:
+                tag_desc_answer[tag] = [q, answer]
 
+                add_question_under_tag(tag_question, tag, q)
+
+        else:
+            pass
 
         # 使用knowledge_id作为tag
 
